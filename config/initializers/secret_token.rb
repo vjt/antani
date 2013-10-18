eed72b6508c2ea7bbc09ea1d64e8459f4ba845fb
Marcello Barnaba <vjt@openssl.it>
2013-10-18 23:09:28 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Antani::Application.config.secret_key_base = '1836243f288ebe962a29d81db96738871db8a81e1669f74388efc9790ecbb05c51c22fcb50fc5e321f149a6bbd0cb357cfe330d3dfd105bd0d27a4d6b74e0643'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Antani::Application.config.session_store :cookie_store, key: '_antani_session'
